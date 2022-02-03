9501b8d30d84b862c538a602dfa226ee83dfa22c
SUN <87660151+soras58@users.noreply.github.com>
2022-02-03 17:27:41 +0900

    Lotto_random
    클릭 하면 재 생성
    중복 확인

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const btn = document.querySelector(".btn");
 const nub = document.querySelector("#number");
 
-var vae = [];
 function clcikz() {
+  var vae = [];
   for (i = 0; i < 6; i++) {
     last = Math.floor(Math.random() * 45) + 1;
-
+    for (var j in vae) {
+      if (last == vae[j]) {
+        last = Math.floor(Math.random() * 45) + 1;
+      }
+    }
     vae.push(last);
   }
 
   vae.sort(function (a, b) {
     return a - b;
   });
+
   nub.innerText = `이번주로또 번호 ${vae}`;
 }
 
